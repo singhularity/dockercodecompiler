5055477fa932230695975fcb461396d6f7a810f0
singhularity <saurabhsin@gmail.com>
2019-11-23 12:08:08 -0500

    Allow server to now accept parameters

--- server/server.go
+++ server/server.go
@@ -1,20 +1,42 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/dockercodecompiler/compiler"
 )
 
-func compile(w http.ResponseWriter, req *http.Request) {
-	runOutput := compiler.Compile([]string{"", "python3", "print('hi')", "hello"})
-	fmt.Fprint(w, runOutput)
+type CompilerParams struct {
+	Language string
+	Code     string
+	Stdin    string
 }
 
+func compile(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		var compilerParams CompilerParams
+		err := json.NewDecoder(req.Body).Decode(&compilerParams)
+		if err == nil {
+			runParams := append([]string{""}, compilerParams.Language, compilerParams.Code, compilerParams.Stdin)
+			fmt.Print(runParams)
+			runOutput := compiler.Compile(runParams)
+			w.Write([]byte(runOutput))
+		} else {
+			w.Write([]byte("Failed to read compiler parameter"))
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Unsupported Method"))
+	}
+}
+
+// Serve Start webserver
 func Serve() {
 
-	http.HandleFunc("/compile", compile)
+	http.HandleFunc("/api/compile", compile)
 
 	http.ListenAndServe(":8090", nil)
 }
